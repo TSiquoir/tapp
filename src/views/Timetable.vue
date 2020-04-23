72eda0a44e262a5ce02ce88242374749d995541f
TSiquoir <archtemps@hotmail.fr>
2020-04-23 10:34:10 +0200

    Ajout d'événement

--- src/views/Timetable.vue
+++ src/views/Timetable.vue
@@ -1,22 +1,25 @@
 <template>
  <v-app id="inspire">
-    <!-- eslint-disable -->
+
     <div>
-      <button @click="events.push({
+      <v-btn class="pink white--text" @click="addEvents ({
       start: '2018-11-20 12:00',
       end: '2018-11-20 17:00',
       title: 'A new event',
       class: 'blue-event'
-      })">Add an event</button>
-      <button @click="events.pop()">Remove last event</button>
+      })">Add an event</v-btn>
+      <v-btn class="pink white--text" @click="events.pop()">Remove last event</v-btn>
 
       <VueCal 
          selected-date="2018-11-19"
-         :time-from="9 * 60"
+         :time-from="8 * 60"
          :time-to="24 * 60"
          :disable-views="['years', 'year', 'month']"
          hide-weekends
          :events="events">
+         <template v-slot:title="{  }">
+           Hello World
+         </template>
       </VueCal>
        
     </div>
@@ -28,12 +31,34 @@
 import VueCal from "vue-cal"
 import 'vue-cal/dist/vuecal.css'
 
+
+
 export default {
-   components: {
-      VueCal,
-     
-   }  
+  components: { 
+    VueCal,
+  },
+  
+  data () {
+    return {
+      events: [
+        {
+          start: '2018-11-19 10:35',
+          end: '2018-11-19 11:30',
+          title: 'Doctor appointment',
+          content: '<i class="v-icon material-icons">local_hospital</i>',
+          class: 'health'
+        },        
+      ]
+    }
+  },
+
+  methods: {
+    addEvents (event) {
+      this.events.push(event)
+    }
+  }
 }
+
 </script>
 
 <style lang="scss" scoped>
